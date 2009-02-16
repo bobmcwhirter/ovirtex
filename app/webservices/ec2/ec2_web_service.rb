a022a32145c4d3e74b4b7bd626d6fa597b01c66a
Bob McWhirter <bob@fnokd.com>
2009-02-16 04:07:25 -0500

    Checking in boilerplate rails bits.

---
+++ app/webservices/ec2/ec2_web_service.rb
@@ -0,0 +1,47 @@
+
+puts "loading EC2WebService"
+
+load "#{RAILS_ROOT}/app/webservices/ec2/base_web_service.rb"
+
+class Ec2WebService < BaseWebService
+
+  target_namespace 'http://ec2.amazonaws.com/doc/2008-12-01/'
+  port_name        'AmazonEC2'
+
+  security do
+    inbound do
+      verify_signature
+      verify_timestamp
+    end
+  end
+
+  def describe_instances()
+    puts "describe_instances #{request}"
+
+    request.instancesSet.each{|item| puts item.instanceId}
+
+    response = create_response
+    response.requestId = "8675309"
+    response
+  end
+
+  def describe_images()
+    puts "describe_images #{request}"
+   
+    response = create_response
+
+    response.requestId = "8675309"
+    response
+  end
+
+  def create_key_pair()
+    puts "create_key_pair #{request}"
+    response = create_response
+    response.requestId = "8675309"
+    response.keyName = request.keyName
+    response.keyFingerprint = "fingerprintofhtekey"
+    response.keyMaterial = "0xkeymaterial"
+    response
+  end
+
+end
